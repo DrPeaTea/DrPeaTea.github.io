c4b16472a889050b400ff11584895a02f9860360
DrPeaTea <110958309+DrPeaTea@users.noreply.github.com>
2023-04-13 17:49:33 +1000

    Update header.tsx

--- src/Components/header.tsx
+++ src/Components/header.tsx
@@ -50,7 +50,8 @@ export default function header() {
               <path d="M0 0h24v1H0zM0 5h24v1H0zM0 10h24v1H0z" />
             </g>
           </svg>
-          <div className="close hidden" onClick={handleMenu}>
+         
+        </div> <div className="close hidden" onClick={handleMenu}>
             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="19">
               <g fill="#2D314D" fill-rule="evenodd">
                 <path d="M.868.661l16.97 16.97-.706.708L.162 1.369z" />
@@ -58,7 +59,6 @@ export default function header() {
               </g>
             </svg>
           </div>
-        </div>
       </div>
     </div>
   );
